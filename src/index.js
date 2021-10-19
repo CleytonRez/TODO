5e0ba411acb72b030debcbfd06619190578e141d
Cleyton Eduardo Rezende <cleytonmgrezendedev@gmail.com>
2021-10-19 19:46:43 -0300

    Post and Put

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import express from "express"
-import todoList from "./models/todolist"
-console.log(express)
-console.log("Hello Word!")
+import todoList from "./models/todoList";
+// console.log(express)
+// console.log("Hello Word!")
 
 
 const app = express();
+app.use(express.json());
 
+// read all
 app.get('/todo', function (req, res) {
     res.json({
         response: todoList
     })
 })
-
+// readbyid
 app.get('/todo/:id', function (request, response) {
     // Pega o ID do path.params.
     const id = request.params.id
@@ -25,5 +27,35 @@ app.get('/todo/:id', function (request, response) {
     })
 
 })
+app.post('/todo', (request, response) => {
+    const body = request.body
+    console.log(body)
+    const id = Math.random()
+    const todo = {
+        _id: id,
+        text: body.text,
+        isChecked: body.isChecked
+    }
+    todoList.push(todo);
+    console.log(todoList)
 
+    response.status(201).end()
+})
+
+app.put('/todo/:id', (response, request) => {
+    const id = request.params.id
+    const body = request.body
+    const newList = todoList.map((obj) => {
+        if (todoList.id === id) {
+            return Object.assign({}, obj, body)
+        }
+        return obj;
+    })
+    todoList = newList
+    response.json({
+        response: id
+    })
+})
+
+console.log('Serve On')
 app.listen(3000)
